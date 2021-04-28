5cd4aa1631485d930eb3a0516f95b5c053ce2201
Tony Burks <tonyburks@Tonys-MacBook-Pro.local>
2021-04-27 23:07:40 -0500

    Added Sign In and Sign Up also adjusted the flex property so it views correctly on tablet and phone

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e7f8e3d35aebfa477a65";
+/******/ 	var hotCurrentHash = "f7b33604f02c2ea3b490";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ pages/signin.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+import SignIn from '../components/SignInLayout/SignIn'
+
+export default function SignInPage() {
+    return (
+        <SignIn />
+    );
+}
