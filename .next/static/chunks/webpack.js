133fec9042ce06e458141d88d739418183dccb45
Tony Burks <tonyburks@me.com>
2021-04-27 23:36:20 -0500

    Added design sign up changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f7b33604f02c2ea3b490";
+/******/ 	var hotCurrentHash = "034d46174d7e9f19e136";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
