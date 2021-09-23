e50ac1f6a441008dbee5e23034c776d1390d150a
Tony Burks <tonyburks@me.com>
2021-09-23 15:57:40 -0500

    Removed Square Payment Form due to depreciation

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react'
 import NavBar from '../components/NavBarLayout/NavBar'
-import PaymentPage from '../components/SquarePaymentFormLayout/PaymentPage'
 
 export default function CheckOutPage() {
   return (
     <>
       <NavBar />
-      <PaymentPage />
     </>
   );
 }
