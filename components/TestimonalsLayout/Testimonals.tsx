8868a950ee3efe9e5975809752d673223b9ee707
Tony Burks <tonyburks@me.com>
2022-01-09 00:18:15 -0600

    changed testimonal picture to have alt text for SEO

--- components/TestimonalsLayout/Testimonals.tsx
+++ components/TestimonalsLayout/Testimonals.tsx
@@ -1,4 +1,4 @@
-import { Box, Circle, Flex, HStack, Text, useColorModeValue } from '@chakra-ui/react'
+import { Box, Circle, Flex, Image, HStack, Text, useColorModeValue } from '@chakra-ui/react'
 import * as React from 'react'
 import Quotee from './Quotee'
 import { QuoteIcon } from './QuoteIcon'
@@ -12,16 +12,16 @@ export default function Testimonals () {
               color={useColorModeValue('gray.300', 'gray.600')}
               fontSize={{ base: '3xl', md: '6xl' }}
             />
-            <Text fontSize={{ base: 'xl', md: '2xl' }} fontWeight="medium" mt="6">
+            <Text fontSize={{ base: 'xl', md: '2xl' }} fontWeight="medium" mt="6" mb="3">
               &ldquo;Burks Pest Control is the best in the business. Very reliable and gets the job done.
               I would give it a 5 star rating over the other pest control businesses in this town.&rdquo;
             </Text>
-            <Quotee
-              name="Amber Burks"
-              jobTitle="Teacher"
-              imageSrc="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OTN8fGxhZHklMjBoZWFkc2hvdCUyMHNtaWxpbmd8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-              mt="8"
-            />
+              <Image
+                src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OTN8fGxhZHklMjBoZWFkc2hvdCUyMHNtaWxpbmd8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+                alt="This is a picture of a random person"
+                borderRadius={'50px'}
+                height={20}
+              />
           </Flex>
           <HStack justify="center" spacing="4" mt="8" color={useColorModeValue('gray.300', 'gray.600')}>
             <Circle w="3" h="3" bg="blue.500" />
