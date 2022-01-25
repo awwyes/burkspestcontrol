910cf9a47d3d588f781c4a9e717daf40b99766ca
Tony Burks <tonyburks@me.com>
2022-01-24 23:31:03 -0600

    Made changes to termite's page

---
+++ components/TermiteLayout/TermiteWhy.tsx
@@ -0,0 +1,68 @@
+import { Container, Stack, Flex, Box, Heading, Text, useColorModeValue, } from '@chakra-ui/react';
+  
+  export default function TermiteWhy() {
+    return (
+        <Box
+            bg={useColorModeValue('white', 'black')}
+        >
+        <Container maxW={'7xl'} >
+        <Stack
+          align={'center'}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 20, md: 28 }}
+          direction={{ base: 'column', md: 'row' }}>
+          <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+            <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}>
+              <Text
+                as={'span'}
+                position={'relative'}
+                >
+                Why Sentricon?
+              </Text>
+            </Heading>
+            <Text color={'gray.500'}>
+                Research indicates the Sentricon® System is the superior choice for termite protection and treatment. 
+                Our entomologists were impressed by the Sentricon® System’s unmatched effectiveness during early trials
+                and, subsequently, Burks Pest Control was chosen among the first authorized installers of Sentricon 
+                during the mid-1990s. Upon seeing a dramatic reduction in new termite infestations, Burks chose to make
+                Sentricon® our company’s exclusive method of termite protection.
+            </Text>
+            <Text color={'gray.500'}>
+                The original Sentricon System® is the only termite product awarded the EPA’s Presidential Green Chemistry
+                Challenge Award. The U.S. Government trusts Sentricon to protect historical landmarks such as Independence
+                Hall, the Statue of Liberty, and the White House. Doesn’t your home deserve the same kind of protection as
+                these national treasures? Call Burks Pest Control today for a free evaluation.
+            </Text>
+          </Stack>
+          <Flex
+            flex={1}
+            justify={'center'}
+            align={'center'}
+            position={'relative'}
+            w={'full'}>
+            <Box
+              position={'relative'}
+              height={'300px'}
+              rounded={'2xl'}
+              boxShadow={'2xl'}
+              width={'full'}
+              overflow={'hidden'}>
+                <iframe 
+                    width="560" 
+                    height="560" 
+                    src="https://www.youtube.com/embed/ECvftMCzh9I" 
+                    title="YouTube video player" 
+                    frameBorder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowFullScreen>
+                </iframe>
+            </Box>
+          </Flex>
+        </Stack>
+      </Container>
+    </Box>
+    );
+}
