40c4ce863ade5e365bc09edcf488414f6b77f5c0
Tony Burks <tonyburks@me.com>
2022-02-22 14:36:47 -0600

    Deleted some pages/api routes

--- components/ResidentialQuoteLayout/ResidentialQuote.tsx
+++ components/ResidentialQuoteLayout/ResidentialQuote.tsx
@@ -1,33 +1,13 @@
-import {
-  Box,
-  ButtonGroup,
-  Divider,
-  Text,
-  Heading,
-  useColorModeValue,
-  FormControl,
-  Select,
-  RadioGroup,
-  Radio,
-  HStack,
-  Button,
-  Input,
-  FormLabel,
-  VStack,
-  Checkbox,
-  CheckboxGroup,
-  SimpleGrid,
-} from "@chakra-ui/react";
-import { Formik } from "formik";
+import { Box, Text, useColorModeValue, Container, Flex, Wrap, WrapItem, Heading } from "@chakra-ui/react";
 import React, { useState } from "react";
 import * as Yup from "yup";
 
 export default function Quote() {
+  const [submitted, setSubmitted] = useState(false);
   const [formValues, setFormValues] = useState({
     firstName: "",
     lastName: "",
     address: "",
-    address2: "",
     city: "",
     state: "",
     zipCode: "",
@@ -36,56 +16,31 @@ export default function Quote() {
     preferContact: "",
     serviceOptions: ["Pest Control"],
   });
+  
 
-  const handleChangeValue = (key) => (event) =>
-    setFormValues({
-      ...formValues,
-      [key]: event.target.value,
-    });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Sending Info");
 
-  const handleChangeCheckboxValue = (key) => (event) =>
-    setFormValues({
-      ...formValues,
-      [key]: event.target.checked,
-    });
+   let data = {
+     formValues
+   }
 
-  const handleClear = () =>
-    setFormValues({
-      firstName: "",
-      lastName: "",
-      address: "",
-      address2: "",
-      city: "",
-      state: "",
-      zipCode: "",
-      phone: "",
-      email: "",
-      preferContact: "",
-      serviceOptions: ["Pest Control"],
-    });
-
-  const onSubmit = (values) => {
-    console.log(values);
-    //Handle submit form data
-    handleClear();
-  };
-
-  const validationSchema = Yup.object({
-    firstName: Yup.string().required(),
-    lastName: Yup.string().required(),
-    address: Yup.string().required(),
-    city: Yup.string().required(),
-    state: Yup.string().required(),
-    zipCode: Yup.number().required(),
-    phone: Yup.string().required(),
-    email: Yup.string()
-      .matches(
-        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      )
-      .required(),
-    preferContact: Yup.string().required(),
-    serviceOptions: Yup.array().required(),
+   fetch("/api/quote", {
+    method: "POST",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((res) => {
+    console.log("Response received");
+    if (res.status === 200) {
+      console.log("Response succeeded!");
+      setSubmitted(true);
+    }
   });
+  };
 
   return (
     <>
@@ -106,112 +61,33 @@ export default function Quote() {
           inspection to determine the proper treatment plan for your property.
         </Text>
       </Box>
-      <Formik
-        initialValues={formValues}
-        onSubmit={onSubmit}
-        validationSchema={validationSchema}
-      >
-        {({ handleSubmit, values, errors }) => (
-          <Box
-            borderWidth="1px"
-            rounded="lg"
-            shadow="1px 1px 3px rgba(0,0,0,0.3)"
-            maxWidth={800}
-            p={6}
-            m="10px auto"
-            as="form"
-            onSubmit={handleSubmit as any}
-            bg={useColorModeValue("white", "black")}
-          >
-            <Heading as={"h3"} fontFamily={"sans-serif"} fontSize={"xl"}>
-              Name
-            </Heading>
-            <Divider />
-            <FormControl isRequired py={3}>
-                <HStack spacing={3}>
-                    <FormLabel htmlFor="firstName">First Name</FormLabel>
-                    <Input name="firstName" label="First Name" w={60} />
-                    <FormLabel htmlFor="lastName">Last Name</FormLabel>
-                    <Input name="lastName" label="Last Name" w={60} />
-                </HStack>
-            </FormControl>
-
-            <Heading as={"h3"} fontFamily={"sans-serif"} fontSize={"xl"} py={3}>
-              Contact Information
-            </Heading>
-            <Divider />
-            <FormControl isRequired py={3}>
-              <FormLabel htmlFor="address">Address</FormLabel>
-              <Input name="address" label="Address" mb={3} />
-              <HStack spacing={2}>
-                <FormLabel htmlFor="city">City</FormLabel>
-                <Input name="city" label="City" w={60} />
-                <FormLabel htmlFor="state">State</FormLabel>
-                <Select name="state" label="State" w={20}>
-                  <option value="AL">AL</option>
-                  <option value="TN" defaultValue={"TN"}>
-                    TN
-                  </option>
-                </Select>
-                <FormLabel htmlFor="zipCode">Zip Code</FormLabel>
-                <Input name="zipCode" label="Zip Code" w={'206px'} />
-              </HStack>
-                <FormLabel htmlFor="phone" py={3}>Phone</FormLabel>
-                <Input name="phone" type="phone" w={80} />
-                <FormLabel htmlFor="email" py={3}>Email</FormLabel>
-                <Input name="email" type="email" w={80} />
-                <FormLabel name="preferContact" py={3}>How would you prefer us to contact you?</FormLabel>
-                <Select
-                    name="preferContact"
-                    selectprops={{
-                        placeholder: "How would you prefer us to contact you?",
-                    }}
-                    w={80}
-                >
-              <option value="Phone" defaultValue={"Phone"}>Phone</option>
-              <option value="Email">Email</option>
-            </Select>
-            </FormControl>
-            <Heading as={"h3"} fontFamily={"sans-serif"} fontSize={"xl"} py={3}>
-              Current Pest Issues
-            </Heading>
-            <Divider />
-            <Text as={'p'} mb={3}>Burks Pest Control Options</Text>
-            <CheckboxGroup>
-              <SimpleGrid columns={2} spacing={3} mb={5}>
-                <Checkbox name="serviceOptions" value="Pest Control">
-                  Pest Control
-                </Checkbox>
-                <Checkbox name="serviceOptions" value="Mosquito Control">
-                  Mosquito Control
-                </Checkbox>
-                <Checkbox name="serviceOptions" value="Other">
-                  Other
-                </Checkbox>
-                <Checkbox name="serviceOptions" value="Termite Control">
-                  Termite Control
-                </Checkbox>
-                <Checkbox name="serviceOptions" value="Bed Bug Removal">
-                  Bed Bug Removal
-                </Checkbox>
-                <Checkbox name="serviceOptions" value="Not Sure">
-                  Not Sure
-                </Checkbox>
-              </SimpleGrid>
-            </CheckboxGroup>
-            
-            <ButtonGroup>
-              <Button>Get My Quote</Button>
-            </ButtonGroup>
-
-            <Box as="pre" marginY={10}>
-              {JSON.stringify(values, null, 2)}
-              <br />
-              {JSON.stringify(errors, null, 2)}
-            </Box>
+      <Container
+      bg={useColorModeValue("white", "black")}
+      maxW="full"
+      mt={0}
+      centerContent
+      overflow="hidden"
+    >
+      <Flex>
+      <Box
+          bg={useColorModeValue("black", "black")}
+          color="white"
+          borderRadius="lg"
+          m={{ sm: 4, md: 16, lg: 10 }}
+          p={{ sm: 5, md: 5, lg: 16 }}
+        >
+          <Box p={4}>
+            <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
+              <WrapItem>
+                <Box>
+                  <Heading>Name</Heading>
+                </Box>
+              </WrapItem>
+            </Wrap>
           </Box>
-        )}
-      </Formik>
+        </Box>
+      </Flex>
+    </Container>
     </>
   );
 }
---
+++ pages/api/quote.js
@@ -0,0 +1,41 @@
+export default function (req, res) {
+    require("dotenv").config();
+
+    const USERNAME = process.env.username;
+    const PASSWORD = process.env.password;
+    const SENDADDRESS = process.env.sendtoaddress;
+
+    let nodemailer = require("nodemailer");
+    const transporter = nodemailer.createTransport({
+    port: 465,
+    service: 'gmail',
+    host: "smtp.gmail.com",
+    auth: {
+      user: USERNAME,
+      pass: PASSWORD,
+    },
+    secure: true,
+  });
+
+  const mailData = {
+    from: `${req.body.email}`,
+    to: SENDADDRESS,
+    subject: "Burks Pest Control Quote Form",
+    context: {
+      name: "Burks Pest Control",
+      company: "Burks Pest Control"
+    },
+    text: `Name: ${req.body.firstName} ${req.body.lastName} + Email: ${req.body.email} + Message: ${req.body.message}`,
+    html: `<div>Name: ${req.body.fullname}</div><br />
+                <p>Email: ${req.body.email}</p><br />
+                <div>Message: ${req.body.message}</div>`,
+  };
+
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) console.log(err);
+    else console.log(info);
+  });
+
+  res.status(200);
+  res.send();
+}
