f7daa48f0cb190034106cb754a9cb3684dd133f6
Tony Burks <tonyburks@me.com>
2022-03-01 13:06:51 -0600

    changed picture on pest control page and refactored the top component of page

--- components/PestControlLayout/PestControl.tsx
+++ components/PestControlLayout/PestControl.tsx
@@ -1,46 +1,54 @@
-import { Box, Flex, chakra, useColorModeValue } from '@chakra-ui/react'
+import {
+  Stack,
+  Flex,
+  Button,
+  Text,
+  VStack,
+  useBreakpointValue,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import * as React from 'react'
 import FreeQuoteButton from '../FreeQuoteButton/FreeQuoteButton'
 
 
 export default function PestControl() {
     return (
-        <Flex
-            backgroundImage={'https://www.cookspest.com/static/7ffb49f4db371ccf0cd0d96eba693de8/front-page-header.jpg'}
-            height={100}
-            w="full"
-        >
-            <Flex
-                justify="left"
-                backgroundImage='url(https://www.cookspest.com/static/7ffb49f4db371ccf0cd0d96eba693de8/front-page-header.jpg)'
-                w="full"
-            >
-                <Box
-                    w={{ base: "full", md: "75%", lg: "50%" }}
-                    px={100}
-                    py={100}
-                    textAlign={{ base: "left", md: "left" }}
-                >
-                    <chakra.span
-                        fontSize={{ base: "3xl", sm: "4xl" }}
-                        fontWeight="extrabold"
-                        letterSpacing="tight"
-                        lineHeight="shorter"
-                        color={useColorModeValue("black", "yellow.500")}
-                        mb={6}
-                    >
-                        <chakra.span display="block" textAlign={'center'}>Pest Control</chakra.span>
-                            <chakra.span
-                                display="block"
-                                fontSize={{ base: "1xl", sm: 'xl' }}
-                                color={useColorModeValue("yellow.500", "black")}
-                            >
-                                Having Pest Issues? Need a real pro to give you a solution?
-                            </chakra.span>
-                        </chakra.span>
-                    <FreeQuoteButton />
-                </Box>
-            </Flex>
-        </Flex>
+    <Flex
+      w={'full'}
+      h={'80vh'}
+      backgroundImage={
+        'url(https://www.gannett-cdn.com/presto/2019/12/05/PNAS/8671ba5f-e918-4f32-882c-d7d3df55b706-6_Black_White_Farmhouse_Carbine__Assoc._Photo_by_Leslie_Brown.jpg?crop=4797,2699,x0,y457&width=1320&height=744&format=pjpg&auto=webp)'
+      }
+      backgroundSize={'cover'}
+      backgroundPosition={'center center'}
+    >
+      <VStack
+        w={'full'}
+        justify={'center'}
+        px={useBreakpointValue({ base: 4, md: 8 })}
+        bgGradient={'linear(to-r, blackAlpha.600, transparent)'}
+        alignItems={'flex-start'}
+      >
+        <Stack align={'flex-start'} spacing={6} mb={10} ml={6}>
+          <Text
+            color={useColorModeValue('white', 'yellow.500')}
+            fontWeight={700}
+            lineHeight={1.2}
+            fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}>
+            Pest Control
+          </Text>
+          <Text
+            color={useColorModeValue('yellow.500','white')}
+            fontWeight={700}
+            lineHeight={1.2}
+            fontSize={useBreakpointValue({ base: 'xl', md: '2xl' })}>
+            Having Pest Issues? Need a real pro to give you a solution?
+          </Text>
+          <Stack direction={'row'}>
+            <FreeQuoteButton />
+          </Stack>
+        </Stack>
+      </VStack>
+    </Flex>
     )
 }
