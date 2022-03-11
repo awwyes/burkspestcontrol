6391ee9da1d1161c9fbabf235cb11785ea5c0c55
Tony Burks <tonyburks@me.com>
2022-03-10 22:20:58 -0600

    Added GA to env and document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
                     {/*Global Site Tag (gtag.js) - Google Analytics */}
                     <script
                         async
-                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.PUBLIC_GOOGLE_ANALYTICS}`}
+                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_G_TAG_ID}`}
                     />
                     <script
                         dangerouslySetInnerHTML={{
@@ -16,7 +16,7 @@ export default class MyDocument extends Document {
                         window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
-                        gtag('config', '${process.env.PUBLIC_GOOGLE_ANALYTICS}', {
+                        gtag('config', '${process.env.GOOGLE_ANALYTICS_G_TAG_ID}', {
                         page_path: window.location.pathname,
                         });
                     `,
