eab31d76daa4a36d9d92736ce4b8412f94460a85
Tony Burks <tonyburks@me.com>
2022-03-23 19:40:50 -0500

    deleted node modules due to erro and reloaded them for chakra ui steps

--- components/CommercialLayouts/CommercialQuote/CommercialStepper.tsx
+++ components/CommercialLayouts/CommercialQuote/CommercialStepper.tsx
@@ -1,3 +1,4 @@
+import { Button, Flex, Heading } from '@chakra-ui/react';
 import { Step, Steps, useSteps } from 'chakra-ui-steps';
 
 const steps = [{ label: "How can we serve you?" }, { label: "Name & Company" }, { label: "Contact Information" }, { label: "Done" } ]
@@ -10,6 +11,40 @@ export const ClickableSteps = () => {
 
 export default function CommericalStepper() {
     return (
-        <div>Something goes here</div>
+        <Flex flexDir="column" width="100%">
+      <Steps onClickStep={(step) => setStep(step)} activeStep={activeStep}>
+        {steps.map(({ label }, index) => (
+          <Step label={label} key={label}>
+              {/* This is where the contents on the page would change for the stepper */}
+            <Contents index={index} /> 
+          </Step>
+        ))}
+      </Steps>
+      {activeStep === steps.length ? (
+        <Flex px={4} py={4} width="100%" flexDirection="column">
+          <Heading fontSize="xl" textAlign="center">
+            Woohoo! All steps completed!
+          </Heading>
+          <Button mx="auto" mt={6} size="sm" onClick={reset}>
+            Reset
+          </Button>
+        </Flex>
+      ) : (
+        <Flex width="100%" justify="flex-end">
+          <Button
+            isDisabled={activeStep === 0}
+            mr={4}
+            onClick={prevStep}
+            size="sm"
+            variant="ghost"
+          >
+            Prev
+          </Button>
+          <Button size="sm" onClick={nextStep}>
+            {activeStep === steps.length - 1 ? "Finish" : "Next"}
+          </Button>
+        </Flex>
+      )}
+    </Flex>
     )
 }
